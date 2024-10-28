735da16e2e9e03ec10e2e29eaa2428fd4163d4af
Felipe Santos <159580121+felipesantos71@users.noreply.github.com>
2024-10-28 12:50:54 +0000

    ajuste de codigo

--- app_banco_sql/main.py
+++ app_banco_sql/main.py
@@ -1,6 +1,7 @@
 from services.usuario_services import UsuarioService
 from repositores.usuario_repositore import UsuarioRepository
 from config.database import Session
+import os
 
 def main():
     session = Session()
@@ -22,4 +23,5 @@ def main():
         print(f"Nome: {usuario.nome} - Email: {usuario.email} - Senha: {usuario.senha}")
 
 if __name__ == "__main__":
+    os.system("cls || clear")
     main() 
