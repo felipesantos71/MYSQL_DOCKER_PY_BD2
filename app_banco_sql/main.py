16d1acfc674422bcce6d72acda723228fc3a8ac7
Felipe Santos <159580121+felipesantos71@users.noreply.github.com>
2024-10-28 12:30:17 +0000

    criando codigo no main

---
+++ app_banco_sql/main.py
@@ -0,0 +1,25 @@
+from services.usuario_services import UsuarioService
+from repositores.usuario_repositore import UsuarioRepository
+from config.database import Session
+
+def main():
+    session = Session()
+    repository = UsuarioRepository(session)
+    service = UsuarioService(repository)
+
+    #Solicitando dados para o usuario
+    nome = input("Digite seu nome: ")
+    email = input("Digite seu email: ")
+    senha = input("Digite seu senha: ")
+
+    service.criar_usuario(nome=nome, email=email, senha=senha)
+
+
+    #Listar todos os usuarios cadastrados
+    print("\nListando usuarios cadastrados.")
+    lista_usuarios = service.listar_todos_usuarios()
+    for usuario in lista_usuarios:
+        print(f"Nome: {usuario.nome} - Email: {usuario.email} - Senha: {usuario.senha}")
+
+if __name__ == "__main__":
+    main() 
